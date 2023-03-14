0366b346c69902e83bcb259199abf0eb39a2d684
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-03-14 16:21:59 +0530

    add

--- sql_lab/src/Main.java
+++ sql_lab/src/Main.java
@@ -1,5 +1,17 @@
+import java.sql.*;
+
+
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/java";
+        String user = "root";
+        String pass = "";
+        try {
+            Connection cn = DriverManager.getConnection(url,user,pass);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("Hello world!");
+
     }
 }
