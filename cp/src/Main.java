148993452d7a8d124d511c54e12170ef67a0554d
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-03-21 15:43:26 +0530

    cp  21

--- cp/src/Main.java
+++ cp/src/Main.java
@@ -13,21 +13,21 @@ public class Main
         l8.setBounds(700,20,200,100);
         f.add(l8);
 
-        JLabel l1 = new JLabel("NAME:");
+        JLabel l1 = new JLabel("Victim NAME:");
         l1.setBounds(600,100,200,40);
         f.add(l1);
         JTextField t1 = new JTextField();
         t1.setBounds(700,100,200,30);
         f.add(t1);
 
-        JLabel l2 = new JLabel("DATE OF BIRTH:");
-        l2.setBounds(600,200,200,40);
+        JLabel l2 = new JLabel("DATE OF BIRTH OF VICTIM:");
+        l2.setBounds(535,200,200,40);
         f.add(l2);
         JTextField t2 = new JTextField();
         t2.setBounds(700,200,200,30);
         f.add(t2);
 
-        JLabel l3 = new JLabel("MOBILE NO:");
+        JLabel l3 = new JLabel("FIR NO:");
         l3.setBounds(600,300,200,40);
         f.add(l3);
         JTextField t3 = new JTextField();
@@ -44,7 +44,7 @@ public class Main
         t6.setBounds(700,440,200,30);
         f.add(t6);
 
-        JLabel l7 = new JLabel("EMAIL-ID");
+        JLabel l7 = new JLabel("ACCUSED NAME ");
         l7.setBounds(600,500,200,40);
         f.add(l7);
         JTextField t7 = new JTextField();
@@ -52,23 +52,16 @@ public class Main
         f.add(t7);
 
 
-        JLabel l4 = new JLabel("ADDRESS:");
-        l4.setBounds(600,570,200,60);
+        JLabel l4 = new JLabel("CRIME COMMITED:");
+        l4.setBounds(580,570,200,60);
         f.add(l4);
         JTextArea t4 = new JTextArea();
         t4.setBounds(700,570,200,50);
         f.add(t4);
 
-        JButton b1 = new JButton(" LOGIN ");
-        b1.setBounds(700,670,100,20);
+        JButton b1 = new JButton(" ADD FIR RECORD ");
+        b1.setBounds(700,670,200,20);
         f.add(b1);
-        JTextField t8 = new JTextField();
-        t8.setBounds(700,670,100,15);
-        f.add(t8);
-
-
-
-
 
         f.setSize(1920,1080);
         f.setLayout(null);
---
+++ cp/src/fir.java
@@ -0,0 +1,117 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class fir extends JFrame {
+    // Define data structure to store FIR records
+    static ArrayList<String> firRecords = new ArrayList<String>();
+
+    // GUI components
+    private JTextField firNumberField;
+    private JTextField victimNameField;
+    private JTextField accusedNameField;
+    private JTextField crimeCommittedField;
+    private JTextArea recordsArea;
+
+    public fir() {
+        // Set up the main window
+        super("FIR System");
+
+        // Create the UI components
+        JLabel firNumberLabel = new JLabel("FIR Number:");
+        firNumberField = new JTextField(10);
+        JLabel victimNameLabel = new JLabel("Victim Name:");
+        victimNameField = new JTextField(20);
+        JLabel accusedNameLabel = new JLabel("Accused Name:");
+        accusedNameField = new JTextField(20);
+        JLabel crimeCommittedLabel = new JLabel("Crime Committed:");
+        crimeCommittedField = new JTextField(20);
+        JButton addButton = new JButton("Add FIR Record");
+        addButton.addActionListener(new AddButtonListener());
+        recordsArea = new JTextArea(10, 30);
+        recordsArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(recordsArea);
+
+        // Add the components to the window
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(5, 5, 5, 5);
+        c.gridx = 0;
+        c.gridy = 0;
+        panel.add(firNumberLabel, c);
+        c.gridx = 1;
+        panel.add(firNumberField, c);
+        c.gridx = 0;
+        c.gridy = 1;
+        panel.add(victimNameLabel, c);
+        c.gridx = 1;
+        panel.add(victimNameField, c);
+        c.gridx = 0;
+        c.gridy = 2;
+        panel.add(accusedNameLabel, c);
+        c.gridx = 1;
+        panel.add(accusedNameField, c);
+        c.gridx = 0;
+        c.gridy = 3;
+        panel.add(crimeCommittedLabel, c);
+        c.gridx = 1;
+        panel.add(crimeCommittedField, c);
+        c.gridx = 1;
+        c.gridy = 4;
+        panel.add(addButton, c);
+        getContentPane().add(panel, BorderLayout.NORTH);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+        // Configure the window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        // Set color for UI components
+        Color lightBlue = new Color(173, 216, 230);
+        panel.setBackground(lightBlue);
+        addButton.setBackground(Color.WHITE);
+        addButton.setForeground(Color.BLACK);
+        recordsArea.setBackground(lightBlue);
+        recordsArea.setForeground(Color.BLACK);
+    }
+
+    // Listener for the "Add FIR Record" button
+    private class AddButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // Collect FIR details from user
+            String firNumber = firNumberField.getText();
+            String victimName = victimNameField.getText();
+            String accusedName = accusedNameField.getText();
+            String crimeCommitted = crimeCommittedField.getText();
+
+            // Create FIR record and add it to the data structure
+            String firRecord = "FIR Number: " + firNumber +
+                    "\nVictim Name: " + victimName +
+                    "\nAccused Name: " + accusedName +
+                    "\nCrime Committed: " + crimeCommitted + "\n";
+            firRecords.add(firRecord);
+            // Update the records area to show the new record
+            recordsArea.setText("");
+            for (String record : firRecords) {
+                recordsArea.append(record + "\n");
+            }
+
+            // Clear the input fields
+            firNumberField.setText("");
+            victimNameField.setText("");
+            accusedNameField.setText("");
+            crimeCommittedField.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new fir();
+    }
+}
+
+
+
